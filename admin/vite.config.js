83bc6a4c11ae87ce80a5431071039ec75784522e
toriqul007 <shohan.hkr@gmail.com>
2022-09-16 09:49:38 +0200

    vvvvvvvvvvv

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   watch: {
-  //     // https://rollupjs.org/guide/en/#watch-options
-  //   }
-  // }
+  build: {
+    watch: false
+  }
 })
